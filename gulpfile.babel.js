566787c309748b61d97602521374409cfa4d1966
mmouchous-ledger <michael.mouchous@ledger.fr>
2024-10-03 11:23:05 +0200

    Format file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,35 +2,35 @@
 "use strict";
 
 // Gulp and node
-const gulp = require( "gulp" );
-const cp = require( "child_process" );
-const notify = require( "gulp-notify" );
-const size = require( "gulp-size" );
+const gulp = require("gulp");
+const cp = require("child_process");
+const notify = require("gulp-notify");
+const size = require("gulp-size");
 
 // Basic workflow plugins
-const browserSync = require( "browser-sync" );
-const browserify = require( "browserify" );
-const source = require( "vinyl-source-stream" );
-const buffer = require( "vinyl-buffer" );
-const clean = require( "gulp-clean" );
-const sass = require( "gulp-sass" );
+const browserSync = require("browser-sync");
+const browserify = require("browserify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const clean = require("gulp-clean");
+const sass = require("gulp-sass");
 const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
 const messages = {
-    jekyllBuild: "<span style=\"color: grey\">Running:</span> $ jekyll build"
+  jekyllBuild: "<span style=\"color: grey\">Running:</span> $ jekyll build"
 };
 
 // Performance workflow plugins
-const htmlmin = require( "gulp-htmlmin" );
-const prefix = require( "gulp-autoprefixer" );
-const sourcemaps = require( "gulp-sourcemaps" );
-const uglify = require( "gulp-uglify" );
-const critical = require( "critical" );
-const sw = require( "sw-precache" );
+const htmlmin = require("gulp-htmlmin");
+const prefix = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const critical = require("critical");
+const sw = require("sw-precache");
 
 // Image Generation
-const responsive = require( "gulp-responsive" );
-const rename = require( "gulp-rename" );
-const imagemin = require( "gulp-imagemin" );
+const responsive = require("gulp-responsive");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
 
 const src = {
   css: "_sass/jekyll-sleek.scss",
@@ -42,56 +42,56 @@ const dist = {
 };
 
 function handleErrors() {
-  var args = Array.prototype.slice.call( arguments );
-  notify.onError( {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
     title: "Compile Error",
     message: "<%= error.message %>"
-  } ).apply( this, args );
-  this.emit( "end" ); // Keep gulp from hanging on this task
+  }).apply(this, args);
+  this.emit("end"); // Keep gulp from hanging on this task
 }
 
 // SASS
-gulp.task( "sass", () => {
-  return gulp.src( src.css )
-    .pipe( sourcemaps.init() )
-    .pipe( sass( {
+gulp.task("sass", () => {
+  return gulp.src(src.css)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
       outputStyle: "compressed",
-      includePaths: [ "scss" ],
+      includePaths: ["scss"],
       onError: browserSync.notify
-    } ).on( "error", sass.logError ) )
-    .pipe( sourcemaps.write( { includeContent: false } ) )
-    .pipe( sourcemaps.init( { loadMaps: true } ) )
-    .pipe( prefix() )
-    .pipe( sourcemaps.write( "./" ) )
-    .pipe( rename( { basename: "main" } ) )
-    .pipe( gulp.dest( dist.css ) )
-    .pipe( browserSync.reload( { stream: true } ) )
-    .pipe( gulp.dest( "assets/css" ) );
-} );
+    }).on("error", sass.logError))
+    .pipe(sourcemaps.write({ includeContent: false }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(prefix())
+    .pipe(sourcemaps.write("./"))
+    .pipe(rename({ basename: "main" }))
+    .pipe(gulp.dest(dist.css))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest("assets/css"));
+});
 
 //  JS
-gulp.task( "js", () => {
-  return browserify( src.js, { debug: true, extensions: [ "es6" ] } )
-    .transform( "babelify", { presets: [ "es2015" ] } )
+gulp.task("js", () => {
+  return browserify(src.js, { debug: true, extensions: ["es6"] })
+    .transform("babelify", { presets: ["es2015"] })
     .bundle()
-    .on( "error", handleErrors )
-    .pipe( source( "bundle.js" ) )
-    .pipe( buffer() )
-    .pipe( sourcemaps.init( { loadMaps: true } ) )
-    .pipe( uglify() )
-    .pipe( sourcemaps.write( "./maps" ) )
-    .pipe( size() )
-    .pipe( gulp.dest( dist.js ) )
-    .pipe( browserSync.reload( { stream: true } ) )
-    .pipe( gulp.dest( "assets/js" ) );
-} );
+    .on("error", handleErrors)
+    .pipe(source("bundle.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write("./maps"))
+    .pipe(size())
+    .pipe(gulp.dest(dist.js))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest("assets/js"));
+});
 
-gulp.task( "critical", done => {
-  critical.generate( {
+gulp.task("critical", done => {
+  critical.generate({
     base: "_site/",
     src: "index.html",
     css: ["assets/css/main.css", "assets/css/tools-suite.css"],
-    dimensions: [ {
+    dimensions: [{
       width: 320,
       height: 480
     }, {
@@ -100,75 +100,75 @@ gulp.task( "critical", done => {
     }, {
       width: 1280,
       height: 960
-    } ],
+    }],
     dest: "../_includes/critical.css",
     minify: true,
     extract: false,
-    ignore: [ "@font-face" ]
-  } );
+    ignore: ["@font-face"]
+  });
   done();
-} );
+});
 
 // Minify HTML
-gulp.task( "html", done => {
-    gulp.src( "./_site/index.html" )
-      .pipe( htmlmin( { collapseWhitespace: true } ) )
-      .pipe( gulp.dest( "./_site" ) );
-    gulp.src( "./_site/*/*html" )
-      .pipe( htmlmin( { collapseWhitespace: true } ) )
-      .pipe( gulp.dest( "./_site/./" ) );
-    done();
-} );
+gulp.task("html", done => {
+  gulp.src("./_site/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./_site"));
+  gulp.src("./_site/*/*html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./_site/./"));
+  done();
+});
 
 // Service Worker
-gulp.task( "sw", () => {
+gulp.task("sw", () => {
   const rootDir = "./";
   const distDir = "./_site";
 
-  return sw.write( `${rootDir}/sw.js`, {
-    staticFileGlobs: [ distDir + "/**/*.{js,html,css,png,jpg,svg}" ],
+  return sw.write(`${rootDir}/sw.js`, {
+    staticFileGlobs: [distDir + "/**/*.{js,html,css,png,jpg,svg}"],
     stripPrefix: distDir
-  } );
-} );
+  });
+});
 
 // Images
-gulp.task( "img", () => {
-  return gulp.src( "_img/posts/*.{png,jpg}" )
-    .pipe( responsive( {
-        "*": [ // For all the images in the posts folder
-          {
-            width: 230,
-            rename: { suffix: "_placehold" }
-          },
-          { // thubmnail
-            width: 535,
-            rename: { suffix: "_thumb" }
-          },
-          { // thumbnail @2x
-            width: 535 * 2,
-            rename: { suffix: "_thumb@2x" }
-          },
-          {
-            width: 575,
-            rename: { suffix: "_xs" }
-          },
-          {
-            width: 767,
-            rename: { suffix: "_sm" }
-          },
-          {
-            width: 991,
-            rename: { suffix: "_md" }
-          },
-          {
-            width: 1999,
-            rename: { suffix: "_lg" }
-          },
-          { // max-width hero
-            width: 1920
-          }
-        ]
-      },
+gulp.task("img", () => {
+  return gulp.src("_img/posts/*.{png,jpg}")
+    .pipe(responsive({
+      "*": [ // For all the images in the posts folder
+        {
+          width: 230,
+          rename: { suffix: "_placehold" }
+        },
+        { // thubmnail
+          width: 535,
+          rename: { suffix: "_thumb" }
+        },
+        { // thumbnail @2x
+          width: 535 * 2,
+          rename: { suffix: "_thumb@2x" }
+        },
+        {
+          width: 575,
+          rename: { suffix: "_xs" }
+        },
+        {
+          width: 767,
+          rename: { suffix: "_sm" }
+        },
+        {
+          width: 991,
+          rename: { suffix: "_md" }
+        },
+        {
+          width: 1999,
+          rename: { suffix: "_lg" }
+        },
+        { // max-width hero
+          width: 1920
+        }
+      ]
+    },
       {
         quality: 70,
         progressive: true,
@@ -176,64 +176,64 @@ gulp.task( "img", () => {
         errorOnEnlargement: false,
         errorOnUnusedConfig: false,
         silent: true
-      } ) )
-      .pipe( imagemin() )
-      .pipe( gulp.dest( "assets/img/posts/" ) );
-} );
+      }))
+    .pipe(imagemin())
+    .pipe(gulp.dest("assets/img/posts/"));
+});
 
 // Build the Jekyll Site
-gulp.task( "jekyll-build", done =>  {
-    browserSync.notify( messages.jekyllBuild );
-    return cp.spawn( jekyll, [ "build" ], { stdio: "inherit" } )
-        .on( "close", done );
-} );
+gulp.task("jekyll-build", done => {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn(jekyll, ["build"], { stdio: "inherit" })
+    .on("close", done);
+});
 
 // Rebuild Jekyll & do page reload
-gulp.task( "rebuild",
-  gulp.series( [ "jekyll-build" ], done => {
+gulp.task("rebuild",
+  gulp.series(["jekyll-build"], done => {
     browserSync.reload();
     done();
-  } )
+  })
 );
 
-gulp.task( "clean", () => {
-  return gulp.src( "_site", { read: false, allowEmpty: true } )
-    .pipe( clean() );
-} );
+gulp.task("clean", () => {
+  return gulp.src("_site", { read: false, allowEmpty: true })
+    .pipe(clean());
+});
 
-gulp.task( "serve", function() {
-  return browserSync( {
+gulp.task("serve", function () {
+  return browserSync({
     server: {
       baseDir: "_site"
     }
-  } );
-} );
+  });
+});
 
-gulp.task( "styles", gulp.series( [ "sass", "critical" ] ) );
+gulp.task("styles", gulp.series(["sass", "critical"]));
 
-gulp.task( "watch", () => {
-  gulp.watch( "_sass/**/*.scss", gulp.series( "styles" ) );
-  gulp.watch( [
+gulp.task("watch", () => {
+  gulp.watch("_sass/**/*.scss", gulp.series("styles"));
+  gulp.watch([
     "*.html",
     "_layouts/*.html",
     "_includes/*.html",
     "_posts/*.md",
     "pages_/*.md",
     "_include/*html"
-  ], gulp.series( "rebuild" ) );
-  gulp.watch( "_js/**/*.js", gulp.series( "js" ) );
-} );
+  ], gulp.series("rebuild"));
+  gulp.watch("_js/**/*.js", gulp.series("js"));
+});
 
-gulp.task( "build", gulp.series( [
+gulp.task("build", gulp.series([
   "clean",
-  gulp.parallel( [ "sass", "js", "img" ] ),
+  gulp.parallel(["sass", "js", "img"]),
   "jekyll-build",
   "critical",
   "sw"
-] ) );
+]));
 
-gulp.task( "default", gulp.series( [
+gulp.task("default", gulp.series([
   "build",
   "serve",
   "watch"
-] ) );
+]));
