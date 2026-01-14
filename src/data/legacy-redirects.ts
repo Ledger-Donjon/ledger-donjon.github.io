3bb4fd5d20368843929757194af8194cf99e3fe6
b0l0k <vincent.bouzon@live.fr>
2026-01-14 23:37:09 +0100

    fix: migrate redirections

---
+++ src/data/legacy-redirects.ts
@@ -0,0 +1,103 @@
+export type LegacyRedirect = {
+  from: string;
+  to: string;
+};
+
+export const legacyRedirects: LegacyRedirect[] = [
+  {
+    from: '/BlackHat2019-presentation',
+    to: 'https://www.ledger.com/blog/BlackHat2019-presentation',
+  },
+  {
+    from: '/Breaking-Trezor-One-with-SCA',
+    to: 'https://www.ledger.com/blog/Breaking-Trezor-One-with-SCA',
+  },
+  {
+    from: '/Capture-the-Fortress',
+    to: 'https://www.ledger.com/blog/Capture-the-Fortress',
+  },
+  {
+    from: '/Ellipal-Security',
+    to: 'https://www.ledger.com/blog/Ellipal-Security',
+  },
+  {
+    from: '/Extracting-Seeds',
+    to: 'https://www.ledger.com/blog/Extracting-Seeds',
+  },
+  {
+    from: '/Introducing-Ledger-Donjon',
+    to: 'https://www.ledger.com/blog/Introducing-Ledger-Donjon',
+  },
+  {
+    from: '/Stealing-all-HTC-Exodus-users',
+    to: 'https://www.ledger.com/blog/Stealing-all-HTC-Exodus-users',
+  },
+  {
+    from: '/Unfixable-Key-Extraction-Attack-on-Trezor',
+    to: 'https://www.ledger.com/blog/Unfixable-Key-Extraction-Attack-on-Trezor',
+  },
+  {
+    from: '/booba_nft',
+    to: 'https://www.ledger.com/blog/booba_nft',
+  },
+  {
+    from: '/coldcard-pin-code',
+    to: 'https://www.ledger.com/blog/coldcard-pin-code',
+  },
+  {
+    from: '/compact-em',
+    to: 'https://www.ledger.com/blog/compact-em',
+  },
+  {
+    from: '/ctf-rainbow',
+    to: 'https://www.ledger.com/blog/ctf-rainbow',
+  },
+  {
+    from: '/ctf-smart-contracts',
+    to: 'https://www.ledger.com/blog/ctf-smart-contracts',
+  },
+  {
+    from: '/ctf-smpc-signature',
+    to: 'https://www.ledger.com/blog/ctf-smpc-signature',
+  },
+  {
+    from: '/donjon-ctf-2021',
+    to: 'https://www.ledger.com/blog/donjon-ctf-2021',
+  },
+  {
+    from: '/ghostbuster',
+    to: 'https://www.ledger.com/blog/ghostbuster',
+  },
+  {
+    from: '/kaspersky-password-manager',
+    to: 'https://www.ledger.com/blog/kaspersky-password-manager',
+  },
+  {
+    from: '/keepkey-side-channel-attack',
+    to: 'https://www.ledger.com/blog/keepkey-side-channel-attack',
+  },
+  {
+    from: '/laser-improvements',
+    to: 'https://www.ledger.com/blog/laser-improvements',
+  },
+  {
+    from: '/oled-vuln',
+    to: 'https://www.ledger.com/blog/oled-vuln',
+  },
+  {
+    from: '/picohsm',
+    to: 'https://www.ledger.com/blog/picohsm',
+  },
+  {
+    from: '/rainbow',
+    to: 'https://www.ledger.com/blog/rainbow',
+  },
+  {
+    from: '/software-wallets',
+    to: 'https://www.ledger.com/blog/software-wallets',
+  },
+  {
+    from: '/ssh-with-tpm',
+    to: 'https://www.ledger.com/blog/ssh-with-tpm',
+  },
+];
